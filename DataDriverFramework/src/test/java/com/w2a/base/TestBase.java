94e349c6b02e13b38ecc908df223b3069a0429d6
Anurag Singh <anurag2apr@gmail.com>
2020-09-30 05:03:43 +0530

    dataprovide changes

--- DataDriverFramework/src/test/java/com/w2a/base/TestBase.java
+++ DataDriverFramework/src/test/java/com/w2a/base/TestBase.java
@@ -1,15 +1,21 @@
 package com.w2a.base;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
+import com.w2a.utilities.ExcelReader;
 
 public class TestBase {
 
@@ -18,6 +24,9 @@ public class TestBase {
 	public static Properties config = new Properties();
 	public static Properties OR = new Properties();
 	public static FileInputStream fis;
+	public static Logger log = LogManager.getLogger("devpinoyLogger");
+	public static ExcelReader excel = new ExcelReader(System.getProperty("user.dir")+"\\src\\test\\resources\\excel\\testdata.xlsx");
+	public static WebDriverWait wait;
 
 	@BeforeSuite
 	public void setUp() throws IOException {
@@ -26,31 +35,50 @@ public class TestBase {
 					System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\Config.properties");
 
 			config.load(fis);
+			log.debug("Config file loaded");
 
 			fis = new FileInputStream(
 					System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\OR.properties");
 
 			OR.load(fis);
+			log.debug("OR file loaded");
 		}
 
 		if (config.getProperty("browser").equals("firefox")) {
 			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+ "\\src\\test\\resources\\executables\\geckodriver.exe");
 			driver = new FirefoxDriver();
+			log.debug("FireFox loaded");
 		} else if (config.getProperty("browser").equals("chrome")) {
 			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+ "\\src\\test\\resources\\executables\\chromedriver.exe");
 			driver = new FirefoxDriver();
+			log.debug("Chrome Launched");
+			
 		}
 		
 		driver.get(config.getProperty("testsiteurl"));
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")),TimeUnit.SECONDS);
+		wait=new WebDriverWait(driver, 5);
 	}
+	
+	public boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		}
+		catch(NoSuchElementException e) {
+			return false;
+		}
+	}
+	
+	
 
 	@AfterSuite
 	public void tearDown() {
 		if(driver!=null) {
 			driver.quit();
 		}
+		log.debug("Test Execution Completed");
 	}
 
 }
--- DataDriverFramework/src/test/java/com/w2a/testcases/BankManagerLoginTest.java
+++ DataDriverFramework/src/test/java/com/w2a/testcases/BankManagerLoginTest.java
@@ -1,15 +1,23 @@
 package com.w2a.testcases;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import com.w2a.base.TestBase;
 
-public class LoginTest extends TestBase {
+public class BankManagerLoginTest extends TestBase {
 	
 	@Test
 	public void loginAsBankManager() throws InterruptedException {
 		driver.findElement(By.xpath(OR.getProperty("bmlBtn"))).click();
-		Thread.sleep(3000);
+		
+		Assert.assertTrue(isElementPresent(By.xpath(OR.getProperty("addCustBtn"))),"Login not successful");
+		//Thread.sleep(3000);
+		log.debug("Login Successfully executed");
+	}
+	
+	@Test
+	public void AddCustomerVerify() {
+		
 	}
 }
